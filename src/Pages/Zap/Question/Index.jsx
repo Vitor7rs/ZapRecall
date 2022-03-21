98a5574d5a81f90686e172b4d72d64f5cf0fa52c
Vitor Macedo <vitorjvbm@gmail.com>
2022-03-21 05:07:38 -0300

    questions show

--- src/Pages/Zap/Question/Index.jsx
+++ src/Pages/Zap/Question/Index.jsx
@@ -3,24 +3,32 @@ import './style.css';
 import setinha from "../../../Assets/setinha.png";
 import { useState } from "react/cjs/react.development";
 
-export default function Question({number, quest}){
+export default function Question({number, quest, res}){
    const [questStatus, setStatus] = useState("close");
-   console.log(questStatus)
     return( 
         <>
-            {questStatus==="close" && 
-                <div className={"question"} onClick={()=> setStatus('open')}>
+            {questStatus === "close" && 
+                <div className={"question"} onClick={()=> setStatus("open")}>
                     <h2>Pergunta {number}</h2>
                     <BiRightArrow />
                 </div>
             }
             {questStatus === "open" &&
-                <div className={"question questionOpen"}>
+                <div className={"question questionOpen"} onClick={()=>setStatus("res")}>
                     <h2>{quest}</h2>
                     <img src={setinha} alt="skip" /> 
                 </div>
             }
-           
+            {questStatus === "res" &&
+                <div className={"question questionOpen res"}>
+                    <h2>{res}</h2>
+                    <div className="optionsContainer">
+                        <div className="errorOption"><span>Não</span><span>lembrei</span></div>
+                        <div className="almostErrorOption"><span>Quase não</span> <span>lembrei</span></div>
+                        <div className="rightOption"><span>Zap!</span></div>
+                    </div>
+                </div>
+            }
         </>
     )
 }
