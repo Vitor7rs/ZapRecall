8543f71f5847edc0a03edd112b303f6cc8b654ee
Matheus Galdino <matheus@respondeai.com.br>
2022-03-24 12:24:14 -0300

    Preparando revisão de código

--- src/Data/Data.js
+++
@@ -1,9 +0,0 @@
-export const questions = [
-    { quest: "O que é JSX?", res: "Uma extensão de linguagem do JavaScript" },
-    { quest: "O React é __ ", res: "uma biblioteca JavaScript para construção de interfaces" },
-    { quest: "Componentes devem iniciar com __", res: "letra maiúscula" },
-    { quest: "Podemos colocar __ dentro do JSX", res: "expressões" },
-    { quest: "O ReactDOM nos ajuda __ ", res: "interagindo com a DOM para colocar componentes React na mesma" },
-    { quest: "Usamos o npm para __", res: "gerenciar os pacotes necessários e suas dependências" },
-    { quest: "Usamos props para __", res: "passar diferentes informações para componentes" }
-];
